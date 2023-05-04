5f8dd52827baff9a19aa9afc4c62b94dfa02e7f7
Tan Eu Zin <taneuzin@u.nus.edu>
2023-05-05 00:32:32 +0800

    Implement random image search

--- ActivityBottomSheet.js
+++ ActivityBottomSheet.js
@@ -2,20 +2,38 @@ import {
   Modal,
   KeyboardAvoidingView,
   StyleSheet,
-  Text,
   View,
   Dimensions,
-  ScrollView,
+  Image,
 } from "react-native";
 import { useState } from "react";
 import NewActivityForm from "./NewActivityForm";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function ActivityBottomSheet({
   open,
   setOpen,
   editable,
   activity,
+  newActivity
 }) {
+  const [imageURI, setImageURI] = useState("");
+  const [modalActivity, setModalActivity] = useState({});
+  const API_ENDPOINT = `https://www.googleapis.com/customsearch/v1?cx=${}&key=${}&searchType=image&fields=items(link)&q=`;
+
+  async function fetchImage() {
+    const response = await fetch(API_ENDPOINT + modalActivity.name);
+    const data = await response.json();
+    const imageUris = data.items.map((item) => item.link);
+    const randomIndex = Math.floor(Math.random() * imageUris.length);
+    const selectedImageUri = imageUris[randomIndex];
+    setImageURI(selectedImageUri);
+  }
+
+  function handleSubmit() {
+      setOpen(false)
+  }
+
   return (
     <Modal
       visible={open}
@@ -24,13 +42,25 @@ export default function ActivityBottomSheet({
       onRequestClose={() => setOpen(false)}
     >
       <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
       >
-          <View style={styles.imageContainer}></View>
-          <View style={styles.descriptionContainer}>
-            <NewActivityForm />
-          </View>
-          <View style={styles.buttonsContainer}></View>
+        <View style={styles.imageContainer}>
+          <TouchableOpacity
+            onPress={fetchImage}
+            style={{ width: "100%", height: "100%" }}
+            disabled={!editable}
+          >
+            <Image
+              style={{ width: "100%", height: "100%" }}
+              source={{ uri: imageURI }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.descriptionContainer}>
+          <NewActivityForm handleSubmit={handleSubmit} setActivity={setModalActivity} activity={modalActivity} editable={editable} newActivity={newActivity}/>
+        </View>
+        <View style={styles.buttonsContainer}></View>
       </KeyboardAvoidingView>
     </Modal>
   );
@@ -47,20 +77,14 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 3,
-    borderWidth: 1,
-    borderColor: "red",
     width: "100%",
   },
   descriptionContainer: {
     height: 0.5 * Dimensions.get("window").height,
-    borderWidth: 1,
-    borderColor: "red",
     width: "100%",
   },
   buttonsContainer: {
     flex: 2,
-    borderWidth: 1,
-    borderColor: "red",
     width: "100%",
   },
 });
