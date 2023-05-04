ee9e0f4b8094dfb6886966ad0d163fa7f0dda00f
Tan Eu Zin <taneuzin@u.nus.edu>
2023-05-05 00:33:53 +0800

    Uses TripContext in HomePage and DayScheduleEditor

--- DayScheduleEditor.js
+++ DayScheduleEditor.js
@@ -1,15 +1,25 @@
 import DraggableCard from "./DraggableCard";
-import { StyleSheet, View, Text, ScrollView, Dimensions, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
 } from "react-native-reanimated";
+import TripContext from "./TripContext";
+import { useContext } from "react";
 
 export default function DayScheduleEditor({ route, navigation }) {
-  const {scheduleArr, date} = route.params
+  const { scheduleArr, date } = route.params;
   const MARGIN = 0.05 * Dimensions.get("window").height;
-  const TITLE_SPACE = 75
-  const SCROLLVIEW_HEIGHT =  scheduleArr.length * (150 + (1.5 * MARGIN)) + TITLE_SPACE;
+  const TITLE_SPACE = 75;
+  const SCROLLVIEW_HEIGHT =
+    scheduleArr.length * (150 + 1.5 * MARGIN) + TITLE_SPACE;
 
   const listToObject = (array) => {
     returnObj = {};
@@ -20,25 +30,40 @@ export default function DayScheduleEditor({ route, navigation }) {
     return returnObj;
   };
 
-  const orderedSchedule = useSharedValue(listToObject(scheduleArr));
-
-  console.log(scheduleArr)
+  const array = useContext(TripContext).data.filter(
+    (day) => day.date == date
+  )[0].schedule;
+  const orderedSchedule = useSharedValue(listToObject(array));
 
   return (
-    <SafeAreaView style={styles.container} >
-    <Animated.ScrollView style={{marginLeft: 0.025 * Dimensions.get("window").width, marginBottom: 10}} contentContainerStyle={[styles.scrollContainer, {minHeight: SCROLLVIEW_HEIGHT}]} showsVerticalScrollIndicator={false} >
-    <Text style={styles.titleText}>{date}</Text>
-      {scheduleArr.map((activity) => {
-        return (
-          <DraggableCard
-            object={activity}
-            id={orderedSchedule.value[activity["name"]]}
-            orderedSchedule={orderedSchedule}
-          />
-        );
-      })}
-    </Animated.ScrollView>
-    </SafeAreaView>
+    <TripContext.Consumer>
+      {({ data, setData }) => (
+        <SafeAreaView style={styles.container}>
+          <Animated.ScrollView
+            style={{
+              marginLeft: 0.025 * Dimensions.get("window").width,
+              marginBottom: 10,
+            }}
+            contentContainerStyle={[
+              styles.scrollContainer,
+              { minHeight: SCROLLVIEW_HEIGHT },
+            ]}
+            showsVerticalScrollIndicator={false}
+          >
+            <Text style={styles.titleText}>{date}</Text>
+            {scheduleArr.map((activity) => {
+              return (
+                <DraggableCard
+                  object={activity}
+                  id={orderedSchedule.value[activity["name"]]}
+                  orderedSchedule={orderedSchedule}
+                />
+              );
+            })}
+          </Animated.ScrollView>
+        </SafeAreaView>
+      )}
+    </TripContext.Consumer>
   );
 }
 
@@ -64,6 +89,5 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: "#3A5A40",
     marginVertical: 10,
-
   },
 });
--- homepage.js
+++ homepage.js
@@ -1,58 +1,37 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef } from "react";
 import { StyleSheet, View, Text, ScrollView, SafeAreaView } from "react-native";
 import DaySection from "./DaySection";
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
+import TripContext from "./TripContext";
 
 const Stack = createStackNavigator();
 
-export default function HomePage({navigation}) {
-    const DATA = [
-        {
-          date: "28-05-2023",
-          schedule: [
-            { type: "destination", name: "Shinjuku", description: "Neon-lit Tokyo playground. Shop, dine, party. Busiest train station in the world. Shinjuku is a must-visit." },
-            { type: "transport", name: "Train to Shibuya", description: "Take the train from Shinjuku station for 8 stops and align at Watashiwa Station" },
-            { type: "destination", name: "Shibuya", description: "Tokyo's iconic intersection. The world's busiest pedestrian crossing, with neon lights and bustling crowds." },
-            { type: "destination", name: "Tokyo Spa Inn", description: "Relax and rejuvenate in the heart of Tokyo. Traditional Japanese baths and massage, with modern amenities. Pure bliss." },
-            { type: "accommodation", name: "Hotel Shibuya", description: "Accommodations for 28th to 30th" },
-    
-          ],
-        },
-        {
-          date: "29-05-2023",
-          schedule: [
-            { type: "destination", name: "Shinjuku" },
-            { type: "transport", name: "busbus" },
-            { type: "destination", name: "Hotel" },
-          ],
-        },
-        {
-          date: "30-05-2023",
-          schedule: [
-            { type: "destination", name: "Shinjuku" },
-            { type: "transport", name: "busbus" },
-            { type: "destination", name: "Hotel" },
-          ],
-        },
-      ];
+export default function HomePage({ navigation }) {
 
-      return (
-        <SafeAreaView style={{backgroundColor: "#A3B18A"}} >
-        <ScrollView showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.container}>
-            {DATA.map((date) => (
-              <DaySection date={date.date} scheduleArr={date.schedule} navigation={navigation} />
-            ))}
+  return (
+    <TripContext.Consumer>{({data, setData}) => (
+      <SafeAreaView style={{ backgroundColor: "#A3B18A" }}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.container}
+        >
+          {data.map((date) => (
+            <DaySection
+              date={date.date}
+              scheduleArr={date.schedule}
+              navigation={navigation}
+            />
+          ))}
         </ScrollView>
-        </SafeAreaView>
-      )
-    }
-
+      </SafeAreaView>)}
+    </TripContext.Consumer>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-      width: "100%",
-      alignItems: "center",
-      backgroundColor: "#A3B18A"
-    },
-  });
+  container: {
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: "#A3B18A",
+  },
+});
